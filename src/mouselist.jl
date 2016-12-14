d507c33f16a5ce81a86db428b3735ffee7528bb1
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2016-12-14 16:02:18 +0000

    corrected mouselist

--- src/mouselist.jl
+++ src/mouselist.jl
@@ -7,4 +7,4 @@ end
 
 # Name of genotype, day of start experiment, mice, valid days:
 const vgat = Gen("vgat", 161205,
-["LO1", "L02","L03", "L04","LO5", "L06","L07", "L08", "L09"], vcat(1:26))
+["L01", "L02","L03", "L04","LO5", "L06","L07", "L08", "L09"], vcat(1:26))
